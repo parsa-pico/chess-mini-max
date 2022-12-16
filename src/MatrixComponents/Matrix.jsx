046b1d7043b89dc224e8f4df2c004f9b33cb36b5
Parsa Foroozmand <parsaforoozmand@gmail.com>
2022-12-16 22:32:32 +0330

    compeleting matrix stuff

--- src/MatrixComponents/Matrix.jsx
+++ src/MatrixComponents/Matrix.jsx
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../Styles/matrix.css";
 import Square from "./Square";
 export default function Matrix() {
   const [showTable, setShowTable] = useState(false);
+  const [selectedSquare, setSelectedSquare] = useState(null);
+  const [solutions, setSolutions] = useState([]);
+  const [matrices, setMatrices] = useState("");
   const [n, setN] = useState();
   const [dimensions, setDimensions] = useState([]);
   const [minCost, setMinCost] = useState([]);
@@ -35,7 +38,14 @@ export default function Matrix() {
     setMinCost(minCost);
     setKLocations(kLocations);
   }, [n]);
-
+  function printMatrices(start, end) {
+    if (end - start === 1) return `A${start + 1}*A${end + 1}`;
+    if (end - start === 0) return `A${start + 1}`;
+    const breakPoint = kLocations[start][end];
+    const firstPart = printMatrices(start, breakPoint);
+    const secondPart = printMatrices(breakPoint + 1, end);
+    return `(${firstPart})*(${secondPart})`;
+  }
   function bottomUpMinCost() {
     const minCostCopy = [...minCost];
     for (let index = 0; index < minCost.length; index++) {
@@ -50,7 +60,7 @@ export default function Matrix() {
     while (step <= n) {
       minCost.forEach((row, rowIndex) => {
         if (rowIndex + step >= n) return;
-        console.log("start:", rowIndex, "end:", rowIndex + step);
+
         const result = minMultipication(rowIndex, rowIndex + step, minCostCopy);
         minCostCopy[rowIndex][rowIndex + step] = result.min;
         kLocationsCopy[rowIndex][rowIndex + step] = result.k;
@@ -85,6 +95,23 @@ export default function Matrix() {
     });
     return { min, k: minK };
   }
+  function printSquareSolution(start, end) {
+    if (end - start === 0) return [];
+    const possibleWays = end - start;
+    const breakPoints = [];
+    for (let k = 0; k < possibleWays; k++) {
+      breakPoints.push(k + start);
+    }
+    const solutions = [];
+    breakPoints.forEach((k) => {
+      solutions.push(
+        `minCost(${start + 1},${k + 1}) + minCost(${k + 2},${
+          end + 1
+        })+p${start}*p${k + 1}*p${end + 1}`
+      );
+    });
+    return solutions;
+  }
   function disableTable() {
     let result = false;
     dimensions.forEach((d) => {
@@ -104,10 +131,13 @@ export default function Matrix() {
           setDimensions([]);
           setKLocations([]);
           setMinCost([]);
+          setMatrices("");
+          setSelectedSquare({});
+          setSolutions([]);
           setN(currentTarget.value);
         }}
       />
-      <div>
+      <div className="matrix__body">
         {n && (
           <span>
             <p>dimensions:</p>
@@ -140,6 +170,13 @@ export default function Matrix() {
                 generate values
               </button>
             )}
+            {kLocations[0][0] !== null && (
+              <div>
+                <button onClick={() => setMatrices(printMatrices(0, n - 1))}>
+                  show matrices
+                </button>
+              </div>
+            )}
             {!showTable && (
               <button
                 className="matrix__btn"
@@ -153,43 +190,67 @@ export default function Matrix() {
             )}
           </>
         )}
-        {showTable &&
-          minCost.map((row, rIndex) => {
-            return (
-              <div className="matrix__row" key={rIndex}>
-                {row.map((column, cIndex) => {
-                  let value = minCost[rIndex][cIndex];
-                  let k = kLocations[rIndex][cIndex];
-                  if (rIndex > cIndex) {
-                    value = "-";
-                    k = null;
-                  }
-                  return (
-                    <span
-                      onClick={() => {
-                        if (rIndex <= cIndex) {
-                          let minCostCopy = [...minCost];
-                          let kLocationsCopy = [...kLocations];
-                          const result = minMultipication(
-                            rIndex,
-                            cIndex,
-                            minCost
-                          );
-                          minCostCopy[rIndex][cIndex] = result.min;
-                          kLocationsCopy[rIndex][cIndex] = result.k;
-                          setMinCost(minCostCopy);
-                          setKLocations(kLocationsCopy);
-                        }
-                      }}
-                      key={rIndex + cIndex}
-                    >
-                      <Square k={k} value={value} />
-                    </span>
-                  );
-                })}
+        <div>{matrices}</div>
+        <div className="matrix__table">
+          <div>
+            {showTable &&
+              minCost.map((row, rIndex) => {
+                return (
+                  <div className="matrix__row" key={rIndex}>
+                    {row.map((column, cIndex) => {
+                      let value = minCost[rIndex][cIndex];
+                      let k = kLocations[rIndex][cIndex];
+                      if (rIndex > cIndex) {
+                        value = "-";
+                        k = null;
+                      }
+                      return (
+                        <span
+                          className="square-wrapper"
+                          onClick={() => {
+                            if (rIndex <= cIndex) {
+                              let minCostCopy = [...minCost];
+                              let kLocationsCopy = [...kLocations];
+                              const result = minMultipication(
+                                rIndex,
+                                cIndex,
+                                minCost
+                              );
+                              minCostCopy[rIndex][cIndex] = result.min;
+                              kLocationsCopy[rIndex][cIndex] = result.k;
+                              setMinCost(minCostCopy);
+                              setKLocations(kLocationsCopy);
+                              setSolutions(printSquareSolution(rIndex, cIndex));
+                              setSelectedSquare({ rIndex, cIndex });
+                            }
+                          }}
+                          key={rIndex + cIndex}
+                        >
+                          <small>{cIndex === 0 && rIndex + 1} </small>
+                          <small className="square-wrapper__cIndex">
+                            {rIndex === 0 && cIndex + 1}
+                          </small>
+                          <Square k={k} value={value} />
+                        </span>
+                      );
+                    })}
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+        {solutions.length !== 0 && (
+          <div>
+            minCost of ({selectedSquare.rIndex + 1},{selectedSquare.cIndex + 1})
+            {solutions.map((solution, index) => (
+              <div key={index}>
+                k={selectedSquare.rIndex + 1 + index} ={">"}
+                {solution}
               </div>
-            );
-          })}
+            ))}
+          </div>
+        )}
+        <div className="push"></div>
       </div>
     </div>
   );
--- src/MatrixComponents/Square.jsx
+++ src/MatrixComponents/Square.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-export default function Square({ value, k }) {
+export default function Square({ value, k, color }) {
   return (
     <span className="matrix-square">
-      <span className="matrix-square__value">{value}</span>
+      <span className={`matrix-square__value matrix-square--${color}`}>
+        {value}
+      </span>
       <span className="matrix-square__k">
         {value !== "-" && value > 0 && k + 1}
       </span>
