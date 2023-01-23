94181646657989ae11bc399239dea1a73a136a93
Parsa Foroozmand <parsaforoozmand@gmail.com>
2023-01-23 15:36:18 +0330

    deleteing matrix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import Board from "./ChessComponents/Board";
-import Home from "./Home";
-import Matrix from "./MatrixComponents/Matrix";
 import "./Styles/app.css";
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/chess" element={<Board />} />
-        <Route path="/matrix" element={<Matrix />} />
+        <Route path="/" element={<Board />} />
       </Routes>
     </div>
   );
